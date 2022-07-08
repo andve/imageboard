669f82dd0cb82a619f3ba46e7c461cfeea713f41
andve <106567655+andve@users.noreply.github.com>
2022-07-08 14:26:19 +0000

    Create Post class and Thread class

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
+board_names = {"tech", "lit", "film"}
+board_list = {
+	"tech": [0, {}],
+	"lit": [0, {}],
+	"film": [0, {}]
+}
+
+class Post:
+	def __init__(self, num, timestamp, name, content, image):
+		self.num = num
+		self.timestamp = timestamp
+		self.name = name
+		self.content = content
+		self.image = image
+
+class Thread(Post):
+	def __init__(self, board, num, timestamp, subject, name, content, image):
+		self.board = board
+		self.subject = subject
+		self.posts = []
+		super().__init__(num, timestamp, name, content, image)
 
 @app.route("/")
 def home():
-	return "home"
+	return render_template("home.html")
 
-@app.route("/<board>")
+@app.route("/<board>", methods=["GET", "POST"])
 def board(board):
 	return board
 
